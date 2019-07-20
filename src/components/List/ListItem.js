995f4a37f5c55c649c578d76d14082d5e89e583b
Sergio Suarez <sergiosgil@gmail.com>
2019-07-21 00:23:51 +0200

    Cambiado el href por un react link

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { Card, Grid } from 'semantic-ui-react';
 import ListItemImg from './ListItemImg';
+import {Link} from 'react-router-dom';
 
 class ListItem extends Component {
   render() {
     return (
       <Grid.Column>
-        <Card href={'/movie/' + this.props.info.id} color='black'>
-          <ListItemImg poster_path={this.props.info.poster_path}/>
-          <Card.Content>
-            <Card.Header>
-              {this.props.info.title}
-            </Card.Header>
-          </Card.Content>
-        </Card>
+        <Link key={this.props.info.id} to={'/movie/' + this.props.info.id}>
+          <Card color='black'>
+            <ListItemImg poster_path={this.props.info.poster_path}/>
+            <Card.Content>
+              <Card.Header>
+                {this.props.info.title}
+              </Card.Header>
+            </Card.Content>
+          </Card>
+        </Link>
+
       </Grid.Column>
     );
   }
